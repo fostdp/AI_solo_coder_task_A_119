a1c15faba7984524a024ef22a9ea789fd217aee2
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-15 07:38:46 +0800

    Update project

--- backend/src/main/java/com/metrology/balance/BalanceSystemApplication.java
+++ backend/src/main/java/com/metrology/balance/BalanceSystemApplication.java
@@ -2,10 +2,14 @@ package com.metrology.balance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
+@EnableConfigurationProperties
 public class BalanceSystemApplication {
 
     public static void main(String[] args) {
